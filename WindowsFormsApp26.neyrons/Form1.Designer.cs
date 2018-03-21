0fb0902bc45161a1befcf16f685a103aa9097987
theLoopa <istatenin@bk.ru>
2018-03-21 19:14:33 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp26.neyrons/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp26.neyrons
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Обязательная переменная конструктора.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Освободить все используемые ресурсы.
+      /// </summary>
+      /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Код, автоматически созданный конструктором форм Windows
+
+      /// <summary>
+      /// Требуемый метод для поддержки конструктора — не изменяйте 
+      /// содержимое этого метода с помощью редактора кода.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         this.pictureBox1 = new System.Windows.Forms.PictureBox();
+         this.timer1 = new System.Windows.Forms.Timer(this.components);
+         this.trackBar1 = new System.Windows.Forms.TrackBar();
+         this.trackBar2 = new System.Windows.Forms.TrackBar();
+         this.trackBar3 = new System.Windows.Forms.TrackBar();
+         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // pictureBox1
+         // 
+         this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+         this.pictureBox1.Location = new System.Drawing.Point(134, 0);
+         this.pictureBox1.Name = "pictureBox1";
+         this.pictureBox1.Size = new System.Drawing.Size(666, 450);
+         this.pictureBox1.TabIndex = 0;
+         this.pictureBox1.TabStop = false;
+         this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+         // 
+         // timer1
+         // 
+         this.timer1.Interval = 20;
+         this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+         // 
+         // trackBar1
+         // 
+         this.trackBar1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+         this.trackBar1.Dock = System.Windows.Forms.DockStyle.Left;
+         this.trackBar1.Location = new System.Drawing.Point(0, 0);
+         this.trackBar1.Maximum = 800;
+         this.trackBar1.Minimum = 1;
+         this.trackBar1.Name = "trackBar1";
+         this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+         this.trackBar1.Size = new System.Drawing.Size(45, 450);
+         this.trackBar1.TabIndex = 1;
+         this.trackBar1.TabStop = false;
+         this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+         this.trackBar1.Value = 100;
+         this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+         // 
+         // trackBar2
+         // 
+         this.trackBar2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+         this.trackBar2.Cursor = System.Windows.Forms.Cursors.Default;
+         this.trackBar2.Dock = System.Windows.Forms.DockStyle.Left;
+         this.trackBar2.Location = new System.Drawing.Point(45, 0);
+         this.trackBar2.Maximum = 1000;
+         this.trackBar2.Minimum = 1;
+         this.trackBar2.Name = "trackBar2";
+         this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+         this.trackBar2.Size = new System.Drawing.Size(45, 450);
+         this.trackBar2.TabIndex = 1;
+         this.trackBar2.TabStop = false;
+         this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+         this.trackBar2.Value = 60;
+         this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+         // 
+         // trackBar3
+         // 
+         this.trackBar3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+         this.trackBar3.Dock = System.Windows.Forms.DockStyle.Left;
+         this.trackBar3.LargeChange = 1;
+         this.trackBar3.Location = new System.Drawing.Point(90, 0);
+         this.trackBar3.Maximum = 1000;
+         this.trackBar3.Name = "trackBar3";
+         this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+         this.trackBar3.Size = new System.Drawing.Size(45, 450);
+         this.trackBar3.TabIndex = 1;
+         this.trackBar3.TabStop = false;
+         this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+         this.trackBar3.Value = 100;
+         this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.trackBar3);
+         this.Controls.Add(this.trackBar2);
+         this.Controls.Add(this.trackBar1);
+         this.Controls.Add(this.pictureBox1);
+         this.Name = "Form1";
+         this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.PictureBox pictureBox1;
+      private System.Windows.Forms.Timer timer1;
+      private System.Windows.Forms.TrackBar trackBar1;
+      private System.Windows.Forms.TrackBar trackBar2;
+      private System.Windows.Forms.TrackBar trackBar3;
+   }
+}
+
---
+++ WindowsFormsApp26.neyrons/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp26.neyrons
+{
+   
+
+   public partial class Form1 : Form
+   {
+      public class Dot
+      {
+         public double x { get; set; }
+         public double y { get; set; }
+         public double dx { get; set; }
+         public double dy { get; set; }
+
+         public Dot(int X, int Y, double F)
+         {
+            x = X;
+            y = Y;
+
+            dx = Math.Sin(F);
+            dy = Math.Cos(F);
+         }
+      }
+
+      static int countDot = 100;
+      static Dot[] h;
+      static int pred = 60; // предел растояния для линий, начиная с которого они будут загаратся
+      
+      static double shift = 1; // шаг точки относительно предыдущего положения
+      static int radKub = 2; // радиус кубиков
+
+      static int intervalTimer = 20;
+      static Bitmap bmp;
+      static Graphics g;
+
+      public void InitializeDots()
+      {
+         h = new Dot[countDot];
+         Random r = new Random();
+         for (int i = 0; i < h.Length; i++)
+            h[i] = new Dot(r.Next(pictureBox1.Width), r.Next(pictureBox1.Height), r.Next(360) * Math.PI / 180);
+      }
+
+      public void InitializeGrapics()
+      {
+         bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+         g = Graphics.FromImage(bmp);
+         g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+      }
+
+      public Form1()
+      {
+         InitializeComponent();
+
+         InitializeGrapics();
+         InitializeDots();
+
+         timer1.Interval = intervalTimer;
+         timer1.Start();
+
+         tablo();
+      }
+
+      // Algoritms
+
+      public void Draw()
+      {
+         g.Clear(Color.White);
+
+         for (int i = 0; i < h.Length; i++)
+         {
+            ref var v = ref h[i];
+
+            g.FillRectangle(new SolidBrush(Color.FromArgb(50, 1, 1, 1)), (int)v.x - radKub, (int)v.y - radKub, 2*radKub, 2*radKub);
+            for (int j = i + 1; j < h.Length; j++)
+            {
+               ref var f = ref h[j];
+               int dist = (int)( Math.Sqrt( Math.Pow(v.x - f.x, 2) + Math.Pow(v.y - f.y, 2) ) );
+               int a = pred - dist;
+               if (a > 3)
+               {
+                  a = a > 255 ? 255 : a;
+                  Pen p = new Pen(new SolidBrush(Color.FromArgb(a, 1, 1, 1)));
+                  p.Width = 2;
+                  g.DrawLine(p, (int)v.x, (int)v.y, (int)f.x, (int)f.y);
+               }
+   
+            }
+
+            // движение
+            v.x += v.dx * shift;
+            v.y += v.dy * shift;
+
+            // замкнуть поле
+            v.x = (v.x > pictureBox1.Width+pred) ? -pred : (v.x < -pred) ? pictureBox1.Width+pred : v.x;
+            v.y = (v.y > pictureBox1.Height+pred) ? -pred : (v.y < -pred) ? pictureBox1.Height+pred : v.y;
+         }
+
+         pictureBox1.Image = bmp;
+      }
+
+      private void tablo()
+      {
+         this.Text = trackBar1.Value + " " + trackBar2.Value + " " + trackBar3.Value;
+      }
+
+      // Events
+
+      private void timer1_Tick(object sender, EventArgs e)
+      {
+         Draw();
+      }
+
+      private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+      {
+         h[1].x = e.X;
+         h[1].y = e.Y;
+      }
+
+      private void Form1_SizeChanged(object sender, EventArgs e)
+      {
+         InitializeGrapics();
+      }
+
+      private void trackBar1_Scroll(object sender, EventArgs e)
+      {
+         countDot = trackBar1.Value;
+         InitializeDots();
+         tablo();
+      }
+
+      private void trackBar2_Scroll(object sender, EventArgs e)
+      {
+         pred = trackBar2.Value;
+         tablo();
+      }
+
+      private void trackBar3_Scroll(object sender, EventArgs e)
+      {
+         shift = trackBar3.Value / 100f;
+      }
+   }
+}
